055b098f4083f8d97dac8fb6eabee1fdc1b188e9
MBHNielsenX <mads.nielsen12@live.dk>
2021-11-23 10:45:32 +0100

    Reader writer

--- src/FileReaderWriter/Reader.java
+++ src/FileReaderWriter/Reader.java
@@ -1,4 +1,16 @@
 package FileReaderWriter;
 
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Reader {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
