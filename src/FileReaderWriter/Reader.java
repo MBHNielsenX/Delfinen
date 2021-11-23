4c3f244d4e826d796b865bf31a0254422bf21734
MBHNielsenX <mads.nielsen12@live.dk>
2021-11-23 10:57:48 +0100

    Reader writer fix

--- src/FileReaderWriter/Reader.java
+++ src/FileReaderWriter/Reader.java
@@ -1,14 +1,15 @@
 package FileReaderWriter;
 
-import java.io.BufferedReader;
-
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Reader {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("output.txt")); //Change filename
+            String line;
+            while((line = reader.readLine()) !=null)
+                System.out.println(reader.readLine());
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/FileReaderWriter/Writer.java
+++ src/FileReaderWriter/Writer.java
@@ -6,6 +6,7 @@ public class Writer {
     public static void main(String[] args) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt")); //Change file name
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
