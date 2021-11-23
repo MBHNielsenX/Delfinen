cb0e9984398d99e3b7f08c1833929940e91d0f3c
MBHNielsenX <mads.nielsen12@live.dk>
2021-11-23 10:48:57 +0100

    first

--- src/FileReaderWriter/Reader.java
+++ src/FileReaderWriter/Reader.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Reader {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt")); //Change filename
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/FileReaderWriter/Writer.java
+++ src/FileReaderWriter/Writer.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Writer {
     public static void main(String[] args) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt")); //Change file name
         } catch (IOException e) {
             e.printStackTrace();
         }
