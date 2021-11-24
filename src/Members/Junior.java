22a44f257f47d947984fba264556d7339b5ade1e
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2021-11-24 11:26:41 +0100

    -Added super attributes, getters and setters -Started cashier methods -Created Pensioner class

--- src/Members/Junior.java
+++ src/Members/Junior.java
@@ -1,7 +1,16 @@
 package Members;
 
+import java.util.Date;
+
 public class Junior extends SwimmingClubMember {
-    public Junior(String name, String address, String email, int phoneNumber, int DD, int MM, int YYYY) {
-        super(name, address, email, phoneNumber, DD, MM, YYYY);
+    public Junior(String name, String address, String email, int phoneNumber, Date dateOfBirth, boolean isActive) {
+        super(name, address, email, phoneNumber, dateOfBirth, isActive);
+        if (isActive){
+            setArrears(1000);
+            setArrears(1000);
+        } else{
+            setArrears(500);
+            setContingent(500);
+        }
     }
 }
--- src/Members/Senior.java
+++ src/Members/Senior.java
@@ -1,7 +1,16 @@
 package Members;
 
+import java.util.Date;
+
 public class Senior extends SwimmingClubMember{
-    public Senior(String name, String address, String email, int phoneNumber, int DD, int MM, int YYYY) {
-        super(name, address, email, phoneNumber, DD, MM, YYYY);
+    public Senior(String name, String address, String email, int phoneNumber, Date dateOfBirth, boolean isActive) {
+        super(name, address, email, phoneNumber, dateOfBirth, isActive);
+        if (isActive){
+            setArrears(1600);
+            setContingent(1600);
+        } else{
+            setArrears(500);
+            setContingent(500);
+        }
     }
 }
