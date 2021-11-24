38f644c08032a96caeba72552e9e55ee00529524
MBHNielsenX <mads.nielsen12@live.dk>
2021-11-24 13:46:48 +0100

    first

--- src/UI/Forsjov.java
+++ src/UI/Forsjov.java
@@ -11,16 +11,15 @@ public class Forsjov {
     public static void main(String[] args) {
 
         Chairman mads = new Chairman("Mads","hej","jens.dk");
-        Senior jens = new Senior("Jens Ryge","Vølundsgade 4","jens@politiken.dk",10203040,"10-5-96");
+
 
         //mads.createNewMember();
 
-        Writer.write(jens);
+
         Reader.printMembersList();
         Reader.printMembersList();
         Reader.findMemberID();
-
-        mads.createNewMember();
+        
 
 
     }
