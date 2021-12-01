5629b25767275ea20d6dd728f2d57753a14b2d52
Meerwaldten <Mikkel_m_j@hotmail.com>
2021-12-01 12:02:58 +0100

    Sorting til coach 2.23

--- src/Users/Coach.java
+++ src/Users/Coach.java
@@ -41,52 +41,27 @@ public class Coach extends Admin{
     public void registerNewTime(ArrayList<SwimmingClubMember> allMembers, ArrayList<SwimmingClubMember> competitiveJuniors, ArrayList<SwimmingClubMember> competitiveSeniors){ //Rename til competition time
         System.out.println("Goddag!");
         Competition currentEvent = Competition.createNewCompetition();
-
         boolean registerMore = true;
-
         while (registerMore) {
             System.out.println("Tast 1 for juniorsvømmere eller 2 for seniorsvømmere!");
             int juniorOrSenior = GetUserInput.integer();
             System.out.println("Hvilken svømmer vil du register en tid til?");
             int swimmerID = GetUserInput.integer();
 
-
             switch (juniorOrSenior) {
                 case 1 -> {
                     SwimmingClubMember currentJuniorMember = MemberHandler.getMemberFromId(swimmerID, competitiveJuniors);
-                    executeMember(currentJuniorMember, juniorOrSenior, swimmerID, competitiveJuniors);
-                }
+                    executeMember(currentJuniorMember, juniorOrSenior, swimmerID, competitiveJuniors);}
                 case 2 -> {
                     SwimmingClubMember currentSeniorMember = MemberHandler.getMemberFromId(swimmerID, competitiveSeniors);
-                    executeMember(currentSeniorMember, juniorOrSenior, swimmerID, competitiveSeniors);
-                }
+                    executeMember(currentSeniorMember, juniorOrSenior, swimmerID, competitiveSeniors);}
             }
-
-            /*
-            SwimmingClubMember currentMember = MemberHandler.getMemberFromId(swimmerID,competitiveSeniors);
-            //SwimmingClubMember currentMember = MemberHandler.getMemberFromId(swimmerID, allMembers);
-            String memberName = currentMember.getName();
-            System.out.println("Hvilken disciplin svømmede svømmeren?");
-            whichStroke(currentMember);
-            System.out.println("Hvilken placering fik svømmeren?");
-            int placering = GetUserInput.integer();
-
-            switch (juniorOrSenior){
-                case 1 -> {FileWriter.writeJuniorComp(currentMember);}
-                case 2 -> {
-                    //FileWriter.writeSeniorComp(currentMember);
-                    FileWriter.updateSeniorsList(competitiveSeniors);
-                }
-            }
-
-             */
-
             registerMore = registerMoreTimes();
         }
     }
+
     public void executeMember(SwimmingClubMember competitiveMember, int juniorOrSenior, int swimmerID, ArrayList<SwimmingClubMember> competitiveMembers) {
         SwimmingClubMember currentMember = MemberHandler.getMemberFromId(swimmerID,competitiveMembers);
-        //SwimmingClubMember currentMember = MemberHandler.getMemberFromId(swimmerID, allMembers);
         String memberName = currentMember.getName();
         System.out.println("Hvilken disciplin svømmede svømmeren?");
         whichStroke(currentMember);
@@ -95,8 +70,7 @@ public class Coach extends Admin{
 
         switch (juniorOrSenior){
             case 1 -> {FileWriter.updateJuniorsList(competitiveMembers);}
-            case 2 -> {FileWriter.updateSeniorsList(competitiveMembers);
-            }
+            case 2 -> {FileWriter.updateSeniorsList(competitiveMembers);}
         }
     }
 
@@ -106,39 +80,18 @@ public class Coach extends Admin{
         return userInput == 1;
     }
 
-
     public void whichStroke(SwimmingClubMember currentMember){
         System.out.println("Tast 1 for butterfly.\nTast 2 for rygsvømning.\nTast 3 for brystsvømning.\nTast 4 for crawl.");
         int userInput = GetUserInput.menu(4);
         System.out.println("Hvilken tid svømmede svømmeren på?");
         double swimTime = GetUserInput.doubl();
         switch (userInput){
-            case 1 -> {
-                currentMember.setButterflyTime(swimTime);
-                //return "butterfly";
-            }
-            case 2 -> {
-                currentMember.setBackstrokeTime(swimTime);
-                //return "rygsvømning";
-            }
-            case 3 -> {
-                currentMember.setBreaststrokeTime(swimTime);
-                //return "brystsvømning";
-            }
-            case 4 -> {
-                currentMember.setFreestyleTime(swimTime);
-                //return "crawl";
-            }
+            case 1 -> {currentMember.setButterflyTime(swimTime);}
+            case 2 -> {currentMember.setBackstrokeTime(swimTime);}
+            case 3 -> {currentMember.setBreaststrokeTime(swimTime);}
+            case 4 -> {currentMember.setFreestyleTime(swimTime);}
         }
-        //return null;
     }
-
-
-
-
-
-
-
 
     public void topFiveResults(ArrayList<SwimmingClubMember> competitiveJuniors, ArrayList<SwimmingClubMember> competitiveSeniors){
         System.out.println("Indtast 1 for at se Juniorsvømmere eller 2 for Seniorsvømmere.");
@@ -151,57 +104,39 @@ public class Coach extends Admin{
                 switch (strokeChoice){
                     case 1 -> {
                         competitiveJuniors.sort(Comparator.comparingDouble(SwimmingClubMember::getButterflyTime));
-                        for (SwimmingClubMember m: competitiveJuniors) {
-                            System.out.println("Svømmer: " + m.getName() + "'s bedste tid i butterfly er: " + m.getButterflyTime() + ".");
-                        }
-                    }
+                        for (SwimmingClubMember m: competitiveJuniors)
+                        {System.out.println("Svømmer: " + m.getName() + "'s bedste tid i butterfly er: " + m.getButterflyTime() + ".");}}
                     case 2 -> {
                         competitiveJuniors.sort(Comparator.comparingDouble(SwimmingClubMember::getBackstrokeTime));
-                        for (SwimmingClubMember m:competitiveJuniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i rygsvømning er: "+m.getBackstrokeTime()+".");
-                        }
-                    }
+                        for (SwimmingClubMember m:competitiveJuniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i rygsvømning er: "+m.getBackstrokeTime()+".");}}
                     case 3 -> {
                         competitiveJuniors.sort(Comparator.comparingDouble(SwimmingClubMember::getBreaststrokeTime));
-                        for (SwimmingClubMember m:competitiveJuniors) {
-                            System.out.println("Svømmer: +"+m.getName()+"'s bedste tid i brystsvømning er: "+m.getBreaststrokeTime());
-                        }
-                    }
+                        for (SwimmingClubMember m:competitiveJuniors)
+                        {System.out.println("Svømmer: +"+m.getName()+"'s bedste tid i brystsvømning er: "+m.getBreaststrokeTime());}}
                     case 4 -> {
                         competitiveJuniors.sort(Comparator.comparingDouble(SwimmingClubMember::getFreestyleTime));
-                        for (SwimmingClubMember m:competitiveJuniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i crawl er: "+m.getFreestyleTime());
-                        }
-                    }
-                }
+                        for (SwimmingClubMember m:competitiveJuniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i crawl er: "+m.getFreestyleTime());}}}
             }
             case 2 -> {
                 switch (strokeChoice) {
                     case 1 -> {
                         competitiveSeniors.sort(Comparator.comparingDouble(SwimmingClubMember::getButterflyTime));
-                        for (SwimmingClubMember m :competitiveSeniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i butterfly er: "+m.getButterflyTime());
-                        }
-
-                    }
+                        for (SwimmingClubMember m :competitiveSeniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i butterfly er: "+m.getButterflyTime());}}
                     case 2 -> {
                         competitiveSeniors.sort(Comparator.comparingDouble(SwimmingClubMember::getBackstrokeTime));
-                        for (SwimmingClubMember m :competitiveSeniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i rygsvømning er: "+m.getBackstrokeTime());
-                        }
-                    }
+                        for (SwimmingClubMember m :competitiveSeniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i rygsvømning er: "+m.getBackstrokeTime());}}
                     case 3 -> {
                         competitiveSeniors.sort(Comparator.comparingDouble(SwimmingClubMember::getBreaststrokeTime));
-                        for (SwimmingClubMember m :competitiveSeniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i brystsvømning er: "+m.getBreaststrokeTime());
-                        }
-                    }
+                        for (SwimmingClubMember m :competitiveSeniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i brystsvømning er: "+m.getBreaststrokeTime());}}
                     case 4 -> {
                         competitiveSeniors.sort(Comparator.comparingDouble(SwimmingClubMember::getFreestyleTime));
-                        for (SwimmingClubMember m :competitiveSeniors) {
-                            System.out.println("Svømmer: "+m.getName()+"'s bedste tid i crawl er: "+m.getFreestyleTime());
-                        }
-                    }
+                        for (SwimmingClubMember m :competitiveSeniors)
+                        {System.out.println("Svømmer: "+m.getName()+"'s bedste tid i crawl er: "+m.getFreestyleTime());}}
                 }
             }
         }
